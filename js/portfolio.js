acec5ba8dfa138673d4c88518bd61957e2e521e7
emilyyujeong <emilyyujeong@gmail.com>
2023-03-14 12:13:51 +0900

    0314

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,7 +1,6 @@
 $(function () {
 
-
-    $('.main').fullpage({
+    var fullpageOption = {
         anchors: [
             'main',
             'portfolio01',
@@ -10,13 +9,61 @@ $(function () {
             'portfolio04',
             'portfolio05'
         ],
-    });
+
+        //navigation: true,
+        onLeave: function (origin, destination, direction) {
+            console.log(origin, destination, direction);
+            $('.gnb li')
+                .eq(destination - 1)
+                .addClass('on')
+                .siblings()
+                .removeClass('on');
+            $('.section').eq(destination - 1)
+                .addClass('on')
+                .siblings()
+                .removeClass('on');
+        },
+
+        afterRender: function () {
+            $('.gnb li')
+                .eq(0)
+                .addClass('on');
+            $('.section')
+                .eq(0)
+                .addClass('on');
+
+        }
+
+    }
+
+
+    $('.main').fullpage(fullpageOption);
+
+
+    function gnb() {
+        var gnbHeight = $('.gnb').height();
+        $('.gnb').css({
+            transform: `translate(0, ${gnbHeight / 2})`
+        })
+    }
+
+    gnb()
 
     $('.mopen').on('click', function () {
-        $(this).toggleClass('on')
+        $(this).toggleClass('on');
+        $('.cover').toggleClass('on');
+        $('.header').toggleClass('on');
     });
 
+    $('.cover li').on('click', function () {
+        $('.cover').removeClass('on');
+        $('.mopen').removeClass('on');
+        $('.header').removeClass('on');
+    });
 
+    $('.cover').on('wheel', function (e) {
+        e.stopPropagation()
+    });
 
 
 })
